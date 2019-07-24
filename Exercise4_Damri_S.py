47465b02c239e1be86643135316dd4f2f5609c47
Damri2534 <52350437+Damri2534@users.noreply.github.com>
2019-07-24 18:15:53 +0700

    Add files via upload

--- Exercise4_Damri_S.py
+++ Exercise4_Damri_S.py
@@ -1,13 +1,9 @@
-a="Foundation English"
-b=80
-c="General Business"
-d=85
-e="Introduction to Computer"
-f=87
-g="Computer Programming"
-h=89
+f=80
+g=86
+i=87
+c=76.5
 print("------Your Score------")
-print(a,":",b)
-print(c,":",d)
-print(e,":",f)
-print(g,":",h)
+print("Foundation English",":",f)
+print("General Business",":",g)
+print("Introduction to Computer",":",i)
+print("Computer Programming",":",c)
